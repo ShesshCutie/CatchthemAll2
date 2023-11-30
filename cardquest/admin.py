1ec2fa990c3a35bfe6ea82c77adfe45580269565
ShesshCutie <141611387+ShesshCutie@users.noreply.github.com>
2023-11-30 05:13:13 +0000

    Add files via upload

---
+++ cardquest/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import  PokemonCard
+
+# Register your models here.
+# admin.site.register(PokemonCard)
+
+@admin.register(PokemonCard)
+class PokemonAdmin(admin.ModelAdmin) :
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
